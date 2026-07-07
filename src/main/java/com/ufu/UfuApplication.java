55e4c6a3d5f3f3e02f6802a403d350251f44f736
권수현 <iokshoi0915@dsm.hs.kr>
2026-07-07 14:09:29 +0900

    chore :: 서버 초기 세팅 yaml,gradle

---
+++ src/main/java/com/ufu/UfuApplication.java
@@ -0,0 +1,13 @@
+package com.ufu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfuApplication.class, args);
+	}
+
+}
